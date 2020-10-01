8469ba91ce7376a74864cffcbc8dfb8b1cd3807e
LM Bibera <lyndon.bibera@headhuntr.io>
2020-10-01 12:35:33 +0800

    Initial Commit

---
+++ StartupBuilder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StartupBuilder
+//
+//  Created by StartupBuilder.INFO on 10/1/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StartupBuilder/StartupBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StartupBuilderApp.swift
+//  StartupBuilder
+//
+//  Created by StartupBuilder.INFO on 10/1/20.
+//
+
+import SwiftUI
+
+@main
+struct StartupBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
